04b83e98890a6d73ad7d67acd2751fa0ab36724d
akak92 <pfdiaz2015@gmail.com>
2024-11-13 16:13:26 -0300

    agrego app.py en servicio API y modifico docker-compose.yml

--- Services/API/app.py
+++ Services/API/app.py
@@ -1,1 +1,11 @@
-print('Hello, World!')
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI(title='API for NLP-Vitae application')
+
+@app.get('/health', summary='Health check endpoint', description="Returns OK value if the service is up.", tags=['Health'])
+def health():
+    return JSONResponse(
+        status_code=200,
+        content={'status' : 'OK'}
+    )
